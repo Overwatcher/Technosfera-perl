cbcd33ebe0b9a9a2adb5aeec8349485dd7f1f0ae
Overwatcher <nazvyacheslav@gmail.com>
2016-04-03 22:49:54 +0300

    Added files via upload

--- homeworks/iter/lib/Local/Iterator.pm
+++ homeworks/iter/lib/Local/Iterator.pm
@@ -3,22 +3,23 @@ package Local::Iterator;
 use strict;
 use warnings;
 
-=encoding utf8
+use Moose;
 
-=head1 NAME
+#sub next {
 
-Local::Iterator - base abstract iterator
+#...;
 
-=head1 VERSION
+#}
 
-Version 1.00
-
-=cut
-
-our $VERSION = '1.00';
-
-=head1 SYNOPSIS
-
-=cut
-
+sub all {
+	my $self = shift;
+	my @all;
+	my ($val, $end);
+	$end = 0;
+	while (!$end) {
+		($val, $end) = $self->next();
+		if ($end != 1) {push(@all, $val);}
+	}
+	return \@all;
+}
 1;
